537a85e8f21602488f4bab9c2453e47d64c31841
Vasily Ermakov <vasily.ermakov@specific-group.com>
2018-01-20 00:50:21 +0300

    Fix access to external /wav directory

--- java/SpeakerRecognizer/src/main/java/speakerid/config/SpringBootConfig.java
+++ java/SpeakerRecognizer/src/main/java/speakerid/config/SpringBootConfig.java
@@ -1,13 +1,34 @@
 package speakerid.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "speakerid")
-public class SpringBootConfig {
+public class SpringBootConfig extends SpringBootServletInitializer {
+
+    @Value("#{systemProperties['os.name'].contains('Windows') ? '${windows.speakers.directory}' : '${unix.speakers.directory}'}")
+    private String speakersDirectory;
+
     public static void main(String args[]) {
         SpringApplication.run(SpringBootConfig.class);
+    }
+
+    @Bean
+    WebMvcConfigurer configurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+                registry.addResourceHandler("/wav/**").addResourceLocations("file:/" + speakersDirectory);
+            }
+        };
     }
 }
