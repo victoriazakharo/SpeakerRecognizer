c7ca76479138c2c1b2c0ad93b100c02fbd4c69f4
victoriazakharo <vicktoriazakharo@gmail.com>
2018-03-30 18:11:27 +0300

    Remove silence from audio

--- java/SpeakerRecognizer/src/main/java/speakerid/controller/HomeController.java
+++ java/SpeakerRecognizer/src/main/java/speakerid/controller/HomeController.java
@@ -171,7 +171,7 @@ public class HomeController {
     private String preprocess(String path) throws IOException, InterruptedException {
         String extensionRegex = "[.][^.]+$";
         String newPath = path.replaceFirst(extensionRegex, "-16k.wav");
-        String cmd = String.format("sox %s -c 1 -r 16000 %s", path, newPath);
+        String cmd = String.format("sox %s -c 1 -r 16000 %s silence 1 0.01 0.03%% -1 0.01 0.03%%", path, newPath);
         Process process = Runtime.getRuntime().exec(cmd);
         process.waitFor();
         return newPath;
